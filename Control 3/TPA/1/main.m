882cd18ea9bbe652d0b3cb98fa703e6e75329209
Tomás Vidal <tomividal9@gmail.com>
2024-11-08 18:26:05 -0300

    feat: modelo FOPDT con un buen error medio cuadrático

--- Control 3/TPA/1/main.m
+++ Control 3/TPA/1/main.m
@@ -2,7 +2,12 @@
 % Control Automático 3
 % Grupo 10
 
-clear all; close all;
+% IMPORTANTE:
+% a data lo normlizo más adelante con respecto a 5v para trabajar entre 0-1
+
+clear all;
+close all;
+s = tf('s');
 
 %% Load file data
 addpath('c:\Users\tomi\Github\facu3\Control 3\TPA\1');
@@ -14,15 +19,19 @@ data = open('prot_sim_data.dat');
 %disp(dataFile.data);
 %size(data.data)
 
+close all;
+
 time = data.data(:,1);
-y6 = data.data(:,7);
+y6 = data.data(:,7)/5; % divido por 5 para normalizar los datos con respecto al pico de 5v
 
 figure();
 plot(time, y6, 'r', 'linewidth', 4);
 hold on;
 grid on;
+title('Data');
 
-lineal_time = [0:1e-3:10];
+time_diff = 1e-3;
+lineal_time = [0:time_diff:10];
 lineal_y6 = zeros(1, length(lineal_time));
 n = 2;
 while_break = 0;
@@ -38,3 +47,43 @@ figure;
 plot(lineal_time, lineal_y6, 'b', 'linewidth', 4);
 hold on;
 grid on;
+title('Data con tiempo lineal');
+
+%% FOPDT
+close all;
+% acomodo los datos
+step_resp = lineal_y6((3/time_diff):(5/time_diff));
+step_resp=step_resp-0.2; % muevo los datos 0.2 porque se comenzó con 20% de duty cycle
+step_resp=step_resp/0.6; % escalo a que la referencia sea 1, porque se puso 80% de duty cycle como step
+step_time = lineal_time(1:length(step_resp));
+step_input = ones(1, length(step_resp)); step_input(1)=0; % esto es para el programa de estimación que necesita una entrada
+
+figure;
+plot(step_time, step_resp, 'b', 'linewidth', 4);
+hold on;
+grid on;
+title('Respuesta al escalón (de 20% a 80% ciclo de trabajo)');
+plot(step_time, step_input, 'r', 'linewidth', 2);
+
+% De los graficos generados se estima el delay
+fopdt_delay = 0.415;
+% De System Identification se obtuvieron los siguientes parámetros
+fopdt_time_constant = 0.28562;
+fopdt_gain = 1;
+
+% Modelo FOPDT resultante
+fopdt_estimated = fopdt_gain/(s*fopdt_time_constant+1);
+fopdt_estimated.inputDelay = fopdt_delay;
+
+[fopdt_est_y, fopdt_est_t] = step(fopdt_estimated, step_time); fopdt_est_y=reshape(fopdt_est_y, size(step_resp));
+
+% figure;
+plot(fopdt_est_t, fopdt_est_y, 'k', 'linewidth', 3);
+% hold on;
+% grid on;
+% title('Modelo FOPDT estimado');
+
+error_fopdt = immse(step_resp, fopdt_est_y);
+disp(sprintf('El error cuadrático medio entre la estimada y los datos es: %f', error_fopdt));
+
+legend('Respuesta al escalón (datos)', 'Escalón', sprintf('Respuesta estimada con modelo FOPDT (%f)', error_fopdt));

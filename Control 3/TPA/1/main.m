21034001844572dd78d69441e8ab3386a57da44e
Tomás Vidal <tomividal9@gmail.com>
2024-11-13 18:45:54 -0300

    feat: haciendo la parte de la PRBS

--- Control 3/TPA/1/main.m
+++ Control 3/TPA/1/main.m
@@ -56,6 +56,29 @@ hold on;
 grid on;
 title('Data con tiempo lineal');
 
+%% Acomodar datos de la respuesta al escalón de subida
+step_resp = lineal_y6((3/time_diff):(5/time_diff));
+step_resp=step_resp-0.2; % muevo los datos 0.2 porque se comenzó con 20% de duty cycle
+step_resp=step_resp/0.6; % escalo a que la referencia sea 1, porque se puso 80% de duty cycle como step
+step_time = lineal_time(1:length(step_resp));
+step_input = ones(1, length(step_resp)); step_input(1)=0; % esto es para el programa de estimación que necesita una entrada
+
+%% Acomodar datos de la respuesta al escalón de bajada
+% step_resp_bajada = lineal_y6((6/time_diff):(8/time_diff));
+% step_resp_bajada=step_resp_bajada-0.2; % muevo los datos 0.2 porque se comenzó con 20% de duty cycle
+% step_resp_bajada=step_resp_bajada/0.6; % escalo a que la referencia sea 1, porque se puso 80% de duty cycle como step
+% step_time_bajada = lineal_time(1:length(step_resp_bajada));
+% step_input_bajada = zeros(1, length(step_resp_bajada)); step_input_bajada(1)=1; % esto es para el programa de estimación que necesita una entrada
+% 
+% fig = figure; set(fig, 'Units', 'Normalized', 'OuterPosition', [0 0 1 1]); set(fig, 'Toolbar', 'none', 'Menu', 'none');
+% plot(step_time_bajada, step_resp_bajada, 'b', 'linewidth', 6);
+% hold on;
+% grid on;
+% title('Respuesta al escalón (bajada)');
+% xlabel('Tiempo [Segundos]');
+% ylabel('Tensión normalizada (a 5V) [Volts]');
+% plot(step_time_bajada, step_input_bajada, '--r', 'linewidth', 2);
+
 %% FOPDT
 close all;
 % acomodo los datos
@@ -120,7 +143,7 @@ fprintf('La mejora del modelo SOPDT contra FOPDT es de "%f%%"\n', (error_fopdt-e
 R1=10e3;
 C1=10e-6;
 complete_model = 1/((R1*C1*s+1)^6);
-complete_model.InputDelay = 0.0765;
+complete_model.InputDelay = 0.0753;
 
 [ec_diff_y, ec_diff_t] = step(complete_model, step_time); ec_diff_y=reshape(ec_diff_y, size(step_resp));
 plot(ec_diff_t, ec_diff_y, 'g', 'linewidth', 2);
@@ -129,3 +152,46 @@ error_ec_diff = immse(step_resp, ec_diff_y);
 fprintf('El error cuadrático medio (SOPDT) entre la estimada y los datos es: %f\n', error_ec_diff);
 
 legend('Respuesta al escalón (datos)', 'Escalón', sprintf('Respuesta estimada con modelo FOPDT (%f)', error_fopdt), sprintf('Respuesta estimada con modelo SOPDT (%f)', error_sopdt), sprintf('Modelo a partir de las ec. en diferencias (%f)\n', error_ec_diff));
+
+%% Parte de la PRBS
+data_prbs = open('prot_sim_data_prbs.dat');
+
+%%
+close all;
+MAX_TIME=80;
+
+time_prbs = data_prbs.data(:,1);
+pwm_prbs = data_prbs.data(:,2)/5;  % divido por 5 para normalizar los datos con respecto al pico de 5v
+y6_prbs = data_prbs.data(:,3)/5; % divido por 5 para normalizar los datos con respecto al pico de 5v
+
+figure();
+plot(time_prbs, y6_prbs, 'r', 'linewidth', 3);
+hold on;
+grid on;
+title('Data PRBS');
+plot(time_prbs, pwm_prbs, 'b', 'linewidth', 1);
+
+lineal_time_prbs = 1:time_diff:MAX_TIME;
+lineal_pwm_prbs = interp1(time_prbs(1:10:length(time_prbs)), pwm_prbs(1:10:length(time_prbs)), lineal_time_prbs);
+lineal_y6_prbs = interp1(time_prbs(1:10:length(time_prbs)), y6_prbs(1:10:length(time_prbs)), lineal_time_prbs);
+
+figure;
+plot(lineal_time_prbs, lineal_y6_prbs, 'r', 'linewidth', 4);
+hold on;
+grid on;
+title('Data PRBS con tiempo lineal');
+plot(lineal_time_prbs, lineal_pwm_prbs, 'b', 'linewidth', 2);
+
+%% Transformada de fourier de la PRBS
+close all;
+plot(lineal_pwm_prbs)
+tf_prbs = fft(lineal_pwm_prbs);
+tf_sys = fftn(lineal_y6_prbs);
+w = 1:length(tf_prbs);
+
+figure;
+plot(abs(tf_prbs), 'b', 'linewidth', 2);
+hold on;
+grid on;
+title('FFT PRBS');
+plot(tf_sys, 'r', 'linewidth', 2);

7188bf9a47e69aa61a0198917a5e03f9c2ac5fd4
Tomás Vidal <tomividal9@gmail.com>
2024-12-04 22:46:00 -0300

    feat: falta poco para la entrega del TP4 de SOyR

--- SOyR/practica_4/entregable_codigo/include/definitions_client.h
+++ SOyR/practica_4/entregable_codigo/include/definitions_client.h
@@ -1,8 +1,11 @@
-#define END_WORD "FIN"
 #define PUB_KEY_A ((int)4)
 #define PUB_KEY_B ((int)5)
 #define PRIV_KEY_A ((int)7)
 #define PRIV_KEY_B ((int)19)
+
+// se encarga de terminar correctamente el servidor
+// si el usuario termina el programa cuando corre
+void handle_termination();
 
 // Se encargar de la decriptación de los datos
 int decipher(int x);
--- SOyR/practica_4/entregable_codigo/include/definitions_server.h
+++ SOyR/practica_4/entregable_codigo/include/definitions_server.h
@@ -1,6 +1,11 @@
+// #define DEFAULT_ADDR "127.0.0.0"
 #define PORT 8080
-#define END_WORD "FIN"
+
 #define TOTAL_NUMS 26
+
+// se encarga de terminar correctamente el servidor
+// si el usuario termina el programa cuando corre
+void handle_termination();
 
 // se encarga de encriptar la info
 int encrypt(int x, int key_a, int key_b);

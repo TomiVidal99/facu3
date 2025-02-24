20cff7ab870deefa035d0d7ddebe9cfe13327f0f
Tomás Vidal <tomividal9@gmail.com>
2025-02-24 08:49:37 -0300

    Hice modelo orden 1 con systemIdentification

--- Control 3/TPA/2/codigo/include/PID.h
+++ Control 3/TPA/2/codigo/include/PID.h
@@ -1,12 +1,12 @@
 #ifndef PID_H
 #define PID_H
 
-const float gains_e[3] = { 1.066741, -2.038646, 0.973957};
+const float gains_e[3] = { 0.678900, -1.211684, 0.535968};
 
-const float gains_u[2] = { -1.875236, 0.875236};
+const float gains_u[2] = { -1.779443, 0.779443};
 
-const float gains_re[2] = { 0.100000, -0.042530};
+const float gains_re[2] = { 0.100000, 0.094138};
 
-const float gains_rs = -0.942530;
+const float gains_rs = -0.805862;
 
 #endif // PID_H
